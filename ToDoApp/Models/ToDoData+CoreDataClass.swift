0de18c342169197070c39c2d06f147c5ed78cd88
Jun <fixme1537@gmail.com>
2023-01-21 16:00:36 +0900

    ToDoApp
    - CoreData Practice
    - AutoLayout
    - TableView and TableViewCell

---
+++ ToDoApp/Models/ToDoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoData+CoreDataClass.swift
+//  ToDoApp
+//
+//  Created by 김준혁 on 2023/01/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoData)
+public class ToDoData: NSManagedObject {
+
+}
---
+++ ToDoApp/Views/ToDoCell.swift
@@ -0,0 +1,69 @@
+//
+//  ToDoCell.swift
+//  ToDoApp
+//
+//  Created by 김준혁 on 2023/01/21.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    
+    @IBOutlet weak var bgColor: UIView!
+    @IBOutlet weak var toDoTextLabel: UILabel!
+    @IBOutlet weak var dateTextLabel: UILabel!
+    @IBOutlet weak var updateButton: UIButton!
+    
+    
+    
+   
+    // ToDoData를 전달받을 변수 (전달 받으면 ==> 표시하는 메서드 실행) ⭐️
+    var toDoData: ToDoData? {
+        didSet {
+            configureUIwithData()
+        }
+    }
+    
+    // (델리게이트 대신에) 실행하고 싶은 클로저 저장
+    // 뷰컨트롤러에 있는 클로저 저장할 예정 (셀(자신)을 전달)
+    var updateButtonPressed: (ToDoCell) -> Void = { (sender) in }
+    
+    // 스토리보드의 생성자
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureUI()
+    }
+    
+    // 기본 UI
+    func configureUI() {
+        bgColor.clipsToBounds = true
+        bgColor.layer.cornerRadius = 8
+        
+        updateButton.clipsToBounds = true
+        updateButton.layer.cornerRadius = 10
+    }
+    
+    // 데이터를 가지고 적절한 UI 표시하기
+    func configureUIwithData() {
+        toDoTextLabel.text = toDoData?.memoText
+        dateTextLabel.text = toDoData?.dateString
+        guard let colorNum = toDoData?.color else { return }
+        let color = MyColor(rawValue: colorNum) ?? .red
+        updateButton.backgroundColor = color.buttonColor
+        bgColor.backgroundColor = color.backgoundColor
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    
+    @IBAction func updateButtonTapped(_ sender: UIButton) {
+        updateButtonPressed(self)
+
+    }
+    
+}
